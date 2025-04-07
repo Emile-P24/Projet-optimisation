3091801123a1368147d7ba12ae0674dda1ff87cd
Emile Thompson <emile.thompson@etu.minesparis.psl.eu>
2025-04-07 14:48:17 +0200

    question 6

--- optimisation perso.py
+++ optimisation perso.py
@@ -3,7 +3,8 @@ from scipy import optimize
 from casadi import *
 import time
 
-alpha = 1
+alpha = 0.1
+
 c = 0.001*np.array([30,1,1.3,4,1])
 v = np.array([0.9,1.5,1.1])
 d = np.array([400,67,33])
@@ -23,14 +24,22 @@ def h(q,d,alpha):
         h[i] = (q[i]*np.exp(-alpha*q[i])+d[i]*np.exp(-alpha*d[i]))/(np.exp(-alpha*q[i])+np.exp(-alpha*d[i]))
     return h
 
-def profit (c,v,d,A,alpha):
+def profit (c,v,d,A,alpha): # profit théorique calculé pour q=d et r = Aq
     return np.dot(v,np.transpose(h(d,d,alpha))) - numpy.dot(c,np.transpose(achat(d,A)))
 
-def f(qr):
-    return numpy.dot(c,qr[-5:]) - np.dot(v,np.transpose(h(qr[:3],d,alpha)))
+def f(qr): # qr est la concaténation de q et r, dans cet ordre
+    q = qr[:3]
+    r = qr[-5:]
+    return numpy.dot(c,r) - np.dot(v,np.transpose(h(q,d,alpha)))
 
-for i in [0.01,1,10,100,1000,100000]:
-    print(profit(c,v,d,A,i))
+def contrainte1(qr): # On veut r > Aq
+    q = qr[:3]
+    r = qr[-5:]
+    return  r - np.dot(A,q)
+
+def contrainte2(qr):# On veut r et q >0
+    return qr
     
-print(optimize.minimize(f,np.zeros(8)))
+
+print(optimize.minimize(f,np.zeros(8),method='SLSQP', constraints = [{'type':'ineq', 'fun':contrainte1},{'type':'ineq', 'fun':contrainte2}]))
 print(profit(c,v,d,A,alpha))
